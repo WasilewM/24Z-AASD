0489f81f73c434a13a900457d9f9b954b31b208a
Mateusz Wasilewski <mateuszwasilewski77@gmail.com>
2024-12-10 22:20:48 +0100

    Clean main.py

--- src/main.py
+++ src/main.py
@@ -26,21 +26,20 @@ async def main():
     await coordinator.start()
     print("Regional Coordinator started")
 
-    # commented to simplify tests
-    # # coordinates from 10,0 to 20,10
-    # coordinator = RegionalCoordinator(f"regional_coordinator2@{DEFAULT_HOST}", AGENT_PASSWORD, 10, 20, 0, 10)
-    # await coordinator.start()
-    # print("Regional Coordinator started")
+    # coordinates from 10,0 to 20,10
+    coordinator = RegionalCoordinator(f"regional_coordinator2@{DEFAULT_HOST}", AGENT_PASSWORD, 10, 20, 0, 10)
+    await coordinator.start()
+    print("Regional Coordinator started")
 
-    # # coordinates from 0,10 to 10,20
-    # coordinator = RegionalCoordinator(f"regional_coordinator3@{DEFAULT_HOST}", AGENT_PASSWORD, 0, 10, 10, 20)
-    # await coordinator.start()
-    # print("Regional Coordinator started")
+    # coordinates from 0,10 to 10,20
+    coordinator = RegionalCoordinator(f"regional_coordinator3@{DEFAULT_HOST}", AGENT_PASSWORD, 0, 10, 10, 20)
+    await coordinator.start()
+    print("Regional Coordinator started")
 
-    # # coordinates from 10,10 to 20,20
-    # coordinator = RegionalCoordinator(f"regional_coordinator4@{DEFAULT_HOST}", AGENT_PASSWORD, 10, 20, 10, 20)
-    # await coordinator.start()
-    # print("Regional Coordinator started")
+    # coordinates from 10,10 to 20,20
+    coordinator = RegionalCoordinator(f"regional_coordinator4@{DEFAULT_HOST}", AGENT_PASSWORD, 10, 20, 10, 20)
+    await coordinator.start()
+    print("Regional Coordinator started")
 
     user = User(f"user1@{DEFAULT_HOST}", AGENT_PASSWORD)
     await user.start()
